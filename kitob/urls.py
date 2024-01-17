6647f2bdd4c5679cfc6a1471c8f0df7fe6067ef2
asilbek <asilbekmirolimov@gmail.com>
2024-01-17 10:56:30 +0500

    changes

--- kitob/urls.py
+++ kitob/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from kitob.views import home_page, about
+from kitob.views import home_page, about, profile_page, book_list, book_detail
 
 urlpatterns = [
     path('', home_page),
-    path('about/', about)
+    path('about/', about),
+    path('profile/', profile_page),
+
+    path('books/', book_list, name='book_list'),
+    path('books/<int:pk>', book_detail, name='book_detail'),
 ]
--- kitob/views.py
+++ kitob/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from kitob.models import Book
 
 
 # Create your views here.
@@ -9,3 +11,24 @@ def home_page(request):
 
 def about(request):
     return HttpResponse("About sahifasi")
+
+
+def profile_page(request):
+    return HttpResponse("Assalomu aleykum Asilbek aka")
+
+
+def book_list(request):
+    books = Book.objects.all()
+    context = {
+        'kitoblar': books
+    }
+    return render(request, template_name='book_list.html', context=context)
+
+
+def book_detail(request, pk):
+    # book = Book.objects.get(pk=pk)
+    book = get_object_or_404(Book, pk=pk)
+    context = {
+        'kitob': book
+    }
+    return render(request, template_name='book_detail.html', context=context)
